a105ed08b7e7668c4cdae82dff83d987dcb1b43c
codefirst2022 <codefirst2022@gmail.com>
2023-10-16 15:56:11 +0530

    entry

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { AllListComponent } from './pages/all-list/all-list.component';
+
+const routes: Routes = [
+  {
+    path: '**',
+    redirectTo:'registration' 
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: 'List',
+    component: AllListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
